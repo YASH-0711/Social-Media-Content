dfbbca669e753c2bf728471d596cd76d2a85a3d9
yash verma <yashyrr1111@gmail.com>
2025-07-07 11:58:23 +0530

    configration changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Calendar from "./components/Calendar/Calendar";
+import CalendarHeader from "./components/CalendarHeader/CalendarHeader";
+import Controls from "./components/Controls/Controls";
+import * as XLSX from "xlsx";
+import { useCalendar } from "../src/context/CalendarContext";
+import AddDialog from "./components/AddDialog";
 
 function App() {
+  const { events, setShowDialog, showDialog } = useCalendar();
+  const handleExport = () => {
+    const ws = XLSX.utils.json_to_sheet(events);
+    const wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, "Posts");
+
+    XLSX.writeFile(wb, "ContentCalendar.xlsx");
+  };
+
+  const handleAddDialog = () => {
+    setShowDialog(true)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className="top-bar">
+        <div className="top-right-controls">
+          <button onClick={handleAddDialog}>+ New...</button>
+          <button onClick={handleExport}>Excel export</button>
+        </div>
+      </div>
+      <div className="calendar-controls-and-view">
+        <Controls />
+        <CalendarHeader />
+      </div>
+      <Calendar />
+
+       <AddDialog open={showDialog} onClose={() => setShowDialog(false)}/>
     </div>
   );
 }
